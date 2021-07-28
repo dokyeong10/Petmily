31924676a0dd8bdf6fb3b6015c5161e56636f9dc
kjungyoun <rlawjddbs963@naver.com>
2021-07-28 14:57:52 +0900

    S05P12A408-102 back git init

---
+++ petmily_back/src/main/java/com/ssafy/petmily/PetmilyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.petmily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetmilyApplication.class, args);
+    }
+
+}
---
+++ petmily_back/src/test/java/com/ssafy/petmily/PetmilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.petmily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
