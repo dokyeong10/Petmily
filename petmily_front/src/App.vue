78364c072fce8d1448c687b9be3286e8a3b19e3e
devjunku <junku8782@gmail.com>
2021-07-29 16:45:56 +0900

    S05P12A408-144 [Env]: vue setting / navbar

--- petmily_front/src/App.vue
+++ petmily_front/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- main 으로 시작 -->
+  <Main/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './views/main/main.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
