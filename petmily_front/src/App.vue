843befe973e83f6a22b2b450ff39a4de7de28389
devjunku <junku8782@gmail.com>
2021-08-02 17:20:48 +0900

    S05P13A408-46 feat: Loginpage Homepage setting

--- petmily_front/src/App.vue
+++ petmily_front/src/App.vue
@@ -1,10 +1,43 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <span>
+      <router-link to="/home" class="mx-5 text-decoration-none">Home</router-link> 
+      <router-link to="/home" class="mx-5 text-decoration-none">Home</router-link> 
+      <router-link to="/" class="mx-5 text-decoration-none">유기동물</router-link>
+      <router-link to="/" class="mx-5 text-decoration-none">커뮤니티</router-link>
+      <router-link to="/" class="mx-5 text-decoration-none">라이브</router-link>
+    </span>
+    <span v-if="isLogin">
+      <router-link to="/" class="mx-5 text-decoration-none">로그아웃</router-link>
+      <router-link to="/" class="mx-5 text-decoration-none">마이페이지</router-link>
+    </span>
+    <span v-else>
+      <router-link @click="goTologin" to="/login" class="mx-5 text-decoration-none">로그인</router-link>
+      <router-link to="/" class="mx-5 text-decoration-none">회원가입</router-link>
+    </span>
+    <router-view/>
   </div>
-  <router-view/>
+  
 </template>
+<script>
+import { ref, onMounted } from 'vue'
+// import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'App',
+  setup () {
+    const isLogin = ref(false)
+    const router = useRouter()
+    const goToLogin = function () {
+      router.push('/login')
+    }
+
+    onMounted(() => { router.push('/home') }) 
+    return { isLogin, goToLogin}
+  }
+}
+</script>
 
 <style>
 #app {
