b2fc1d1c22333bd6a2d343c7ddfb11c9572e6c0a
kyinl <kyinle2@gmail.com>
2021-08-05 22:21:53 +0900

    S05P13A408-68 feat: animalRegister

---
+++ petmily_front/src/views/animal/AnimalRegisterSuccess.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    동물 등록에 성공하셨습니다.
+  </div>
+</template>
+<script>
+// import { onMounted } from 'vue'
+// import { useStore } from 'vuex'
+// import { useRouter } from 'vue-router'
+export default {
+  name: 'AnimalRegisterSuccess',
+}
+</script>
+<style>
+
+</style>
