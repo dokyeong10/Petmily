b923df276885959e567ed80c1284755b794ade75
devjunku <junku8782@gmail.com>
2021-08-05 20:11:59 +0900

    S05P13A408-73 feat: animlaList_init

---
+++ petmily_front/src/views/animal/AnimalList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AnimalCategory/>
+  </div>
+</template>
+<script>
+import AnimalCategory from '@/views/animal/components/AnimalCategory'
+
+export default {
+  name: 'animallist',
+  components: {
+    AnimalCategory
+  }
+}
+</script>
+<style>
+  
+</style>
