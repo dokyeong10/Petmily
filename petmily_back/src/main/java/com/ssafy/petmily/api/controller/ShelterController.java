b7dee04b94e4ea9764c588dd7ea0bfe24cc342c7
kjungyoun <rlawjddbs963@naver.com>
2021-08-07 21:17:17 +0900

    S05P13A408-85 [feat] : Search Shelter By Name

---
+++ petmily_back/src/main/java/com/ssafy/petmily/api/controller/ShelterController.java
@@ -0,0 +1,31 @@
+package com.ssafy.petmily.api.controller;
+
+import com.ssafy.petmily.api.service.ShelterService;
+import com.ssafy.petmily.db.entity.Shelter;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Api(value = "보호소 API", tags = {"Shelter"})
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/shelter")
+public class ShelterController {
+
+    @Autowired
+    ShelterService shelterService;
+
+    @GetMapping("/{word}")
+    public ResponseEntity<List<Shelter>> searchShelterCode(@PathVariable String word){
+        System.out.println("========================= searchShelterCode 요청 데이터 : " + word);
+        List<Shelter> list = shelterService.searchShelterCode(word);
+        System.out.println("========================= searchShelterCode 응답 데이터 : "+list);
+        return new ResponseEntity<List<Shelter>>(list, HttpStatus.OK);
+    }
+}
---
+++ petmily_back/src/main/java/com/ssafy/petmily/api/service/ShelterService.java
@@ -0,0 +1,10 @@
+package com.ssafy.petmily.api.service;
+
+import com.ssafy.petmily.db.entity.Shelter;
+
+import java.util.List;
+
+public interface ShelterService {
+
+    List<Shelter> searchShelterCode(String word);
+}
