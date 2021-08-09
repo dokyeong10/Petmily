01069e25d4e18307f2671aef4f3ca989a74594bc
kyinl <kyinle2@gmail.com>
2021-08-09 16:27:00 +0900

    S05P13A408-107 feat: Register Improved

--- petmily_front/src/views/accounts/AgencyForm.vue
+++ petmily_front/src/views/accounts/AgencyForm.vue
@@ -1,12 +1,15 @@
 <template>
-  <JumbotronTerms/>
+  <JumbotronForms/>
   <div class="container mb-5">
     <div style="width: 600px;">
-      <label for="agencyname" class="d-flex flex-row">기관 이름</label>
-      <input v-model="state.agencyname" type="text" class="form-control radius-border br mb-5" placeholder="기관 이름" id="agencyname">
-      <label for="agencycode" class="d-flex flex-row">기관 코드</label>
+      <div class="d-flex flex-column align-items-start">
+        <h1 class="overpass">Sign up to Petmily</h1>
+        <h5 class="overpass colored">Agency Member</h5>
+      </div>
+      <br>
+      <label for="agencyname" class="d-flex flex-row" >기관 이름</label>
       <div class="d-flex" style="width: 400px;">
-        <input v-model="state.agencycode" type="text" class="form-control radius-border br mb-5" placeholder="기관 코드" id="agencycode">
+        <input :value="state.agencyname" type="text" class="form-control radius-border br mb-5" placeholder="기관 이름" id="agencyname" data-bs-toggle="modal" data-bs-target="#agencycodeSearchModal">
         <!-- 기관 코드 검색 모달 -->
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agencycodeSearchModal" style="height: 38px; width: 80px">
           검색
@@ -15,7 +18,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="agencycodeSearchModalLabel">기관 코드 검색</h5>
+                <h5 class="modal-title" id="agencycodeSearchModalLabel">기관 정보 검색</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="agencycodeSearchDataDelete"></button>
               </div>
               <div class="modal-body d-flex">
@@ -23,9 +26,8 @@
                 <button type="button" class="btn btn-primary" @click="agencycodeSearch">검색</button>
               </div>
               <ul v-if="state.agencycodeSearchData">
-                  <li v-for="value in state.agencycodeSearchData" :key="value.agencycode">기관 이름: {{ value.agencyname }} | 기관 코드: {{ value.agencycode }}</li>
+                  <li v-for="value in state.agencycodeSearchData" :key="value.agencycode" @click="getAgencyData(value)" data-bs-dismiss="modal" id="myDiv">기관 이름: {{ value.agencyname }} | 기관 코드: {{ value.agencycode }}</li>
               </ul>
-              <div>키워드로 검색하여 기관 코드를 복사 후 붙여넣기해 주세요.</div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="agencycodeSearchDataDelete">닫기</button>
               </div>
@@ -33,6 +35,18 @@
           </div>
         </div>
       </div>
+      <label for="agencycode" class="d-flex flex-row">기관 코드</label>
+      <input :value="state.agencycode" type="text" class="form-control radius-border br mb-5" placeholder="기관 코드" id="agencycode">
+      <!-- 사진등록 -->
+      <div class="mb-4">
+        <label class="d-flex flex-row">사진등록</label>
+        <div class="d-flex justify-content-start">
+          <img v-if="state.imgURL" :src="state.imgURL" />
+          <input id="file-selector" ref="file" type="file" @change="handleFileUpload()" />
+          <button @click="upload" class="btn-up" style="color: #FFFFFF;" flat>업로드</button>
+        </div>
+      </div>
+
       <label for="email" class="d-flex flex-row">이메일</label>
       <span class="d-flex" style="width: 400px;">
         <input v-model="state.email" type="text" class="form-control radius-border br" placeholder="petmily@email.com" @keyup.enter="confirmEmail" id="email" style="height: 38px;">
@@ -50,6 +64,7 @@
       </div>
       <label for="username" class="d-flex flex-row">담당자 이름</label>
       <input v-model="state.username" type="text" class="form-control radius-border br mb-5" placeholder="담당자 이름" id="username">
+      <div v-if="isUsername">담당자 이름은 필수 항목입니다.</div>
       <label for="password" class="d-flex flex-row">비밀번호</label>
       <input v-model="state.password" type="password" class="form-control radius-border br mb-5" placeholder="••••••••" id="password">
       <label for="confirmPassword" class="d-flex flex-row">비밀번호 재입력</label>
@@ -61,17 +76,17 @@
   </div>
 </template>
 <script>
-import {reactive} from 'vue'
+import { ref, reactive } from 'vue'
+import AWS from "aws-sdk"
 import axios from 'axios'
 import { useRouter } from 'vue-router'
-import JumbotronTerms from '@/views/accounts/components/JumbotronTerms'
+import JumbotronForms from '@/views/accounts/components/JumbotronForms'
 export default {
-  components: { JumbotronTerms },
+  components: { JumbotronForms },
   name: 'agencyform',
-  componenet: {
-    JumbotronTerms,
-  },
   setup () {
+    const file = ref(null)
+
     const state = reactive({
       isEmail: false,
       isConfirm: false,
@@ -85,7 +100,13 @@ export default {
       phone: "",
       agencycodeSearchKeyword: "",
       agencycodeSearchData: "",
-      isClickedSearch: false,
+      
+      file: null,
+      albumBucketName: "petmily",
+      bucketRegion: "ap-northeast-2",
+      IdentityPoolId: "ap-northeast-2:50493919-440f-47aa-8403-c78182e3ed3e",
+      imgURL: "",
+      
     })
     const router = useRouter()
     const setToken = function () {
@@ -148,10 +169,15 @@ export default {
       const reg = /(?=.*[a-zA-ZS])(?=.*?[#?!@$%^&*-]).{8,16}/
       const reg_required = /.{1,}/
       const reg_phone = /^\d{3}-\d{3,4}-\d{4}$/
-      const reg_name = /^[가-힣]+$/
-
+      // 기관 이름: 한글과 공백 허용
+      const reg_name = /^[가-힣\s]+$/
+      
       if (!reg_name.test(state.agencyname)) {
         return alert("기관 이름은 필수 항목입니다.")
+      }
+
+      if (state.file && !state.imgURL) {
+        return alert("업로드 버튼을 눌러주세요.")
       }
 
       if (!state.isConfirm) {
@@ -216,12 +242,60 @@ export default {
       state.agencycodeSearchKeyword = ""
       state.agencycodeSearchData = ""
     }
-    return {state, confirmSignup, setToken, confirmEmail, confirmNumber, agencycodeSearch, agencycodeSearchDataDelete }
+    // 이미지 업로드 구문 ///
+    const handleFileUpload = function() {
+      state.file = file.value.files[0];
+    };
+
+    const upload = function() {
+      AWS.config.update({
+        region: state.bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: state.IdentityPoolId,
+        }),
+      });
+
+      const s3 = new AWS.S3({
+        apiVersion: "2006-03-01",
+        params: {
+          Bucket: state.albumBucketName,
+        },
+      });
+      let photoKey = state.file.name;
+      s3.upload(
+        {
+          Key: photoKey,
+          Body: state.file,
+          ACL: "public-read",
+        },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+            return alert("에러가 발생했습니다.", err.message);
+          }
+          state.imgURL = data.Location;
+        }
+      );
+    };
+
+    const getAgencyData = function (value) {
+      state.agencycode = value.agencycode
+      state.agencyname = value.agencyname
+    }
+
+    return {state, confirmSignup, setToken, confirmEmail, confirmNumber, agencycodeSearch, agencycodeSearchDataDelete, handleFileUpload, upload, file, getAgencyData}
   }
   
 }
 </script>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Overpass:wght@700&display=swap');
+.overpass {
+  font-family: 'Overpass', sans-serif;
+}
+.colored {
+  color: #B8B6F9
+}
 .br {
   border-radius: 12px;
   border-right: #789ADE 1px solid;
@@ -250,5 +324,14 @@ export default {
   width: 400px;
   color: #789ADE;
 }
-
+ul{
+  list-style: none;
+}
+#myDiv
+{
+  cursor: pointer;
+}
+#myDiv:hover {
+  text-decoration: underline;
+}
 </style>
