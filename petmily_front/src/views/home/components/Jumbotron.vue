421ec846d19a6c76df5687c689b139a1a7b8eddf
kt5965 <kth5965@gmail.com>
2021-08-09 17:03:31 +0900

    S05P13A408-98 feat:email_password_styling

--- petmily_front/src/views/home/components/Jumbotron.vue
+++ petmily_front/src/views/home/components/Jumbotron.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="Jbgc container" style="height: 350px;">
-    <div class="d-flex justify-content-even">
-      <div>
-        
+  <div class="Jbgc" style="height: 761px;">
+    <br>
+    <div class="container">
+      <div class="d-flex justify-content-center">
+      <div class="d-flex flex-column">
+        <div class="ms-5 mt-5 font-bold" align="left" style="font-size: 26px;color: #AB0B14;">Our Vision</div>
+        <div class="ms-5 font-bold" align="left" style="font-size: 50px;">지금 5,123 마리의 생명이</div>
+        <div class="ms-5 mb-3 font-bold" align="left" style="font-size: 50px;">가족을 기다립니다.</div>
+        <div class="ms-5" align="left" style="font-size: 16px;">많은 생명들이 가족을 잃고 버려지고 있습니다.</div>
+        <div class="ms-5 mb-5" align="left" style="font-size: 16px;">여러분들의 새로운 가족이 되고 싶어해요</div>
+        <div class="d-flex">
+          <img class="ms-5" align="left" src="@\assets\checkButton.png" style="width: 19px; height: 19px;">
+          <span class="ms-3" style="font-size: 17px;"> 많은 동물들의 보금자리를 찾을 것입니다.</span>
+        </div>
+        <div class="d-flex">
+          <img class="ms-5" align="left" src="@\assets\checkButton.png" style="width: 19px; height: 19px;">
+          <span class="ms-3" style="font-size: 17px;"> 유기율이 낮아지는 그날까지</span>
+        </div>
+        <div class="d-flex">
+          <img class="ms-5" align="left" src="@\assets\checkButton.png" style="width: 19px; height: 19px;">
+          <span class="ms-3" style="font-size: 17px;"> 새로운 가족이 되어주세요!</span>
+        </div>
       </div>
-      <div>
-        
-      </div>
+      <img src="@\assets\dog.png" style="margin-top: 15rem;width: 500px; height: 300px; border-radius: 30px;">
+     </div>
     </div>
   </div>
 </template>
@@ -18,5 +35,9 @@ export default {
 <style>
 .Jbgc {
   background-color: #FAF9FE;
+}
+
+.font-bold {
+  font-weight: bold;
 }
 </style>
